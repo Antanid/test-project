639283325e6d49806dbb113f2ed4a0ae3e42f35a
Antanid <dbalakleenko1@gmail.com>
2025-04-11 11:59:42 +0300

    feat: updated HomeScreen with opacity animation

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = function (api) {
     ],
 
     plugins: [
+      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
--- screens/home/home.tsx
+++ screens/home/home.tsx
@@ -13,10 +13,13 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { FlashList } from '@shopify/flash-list'
 import { useEffect, useState } from 'react'
 import { ActivityIndicator, SafeAreaView, View } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 import colors from 'tailwindcss/colors'
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>()
+
+  const opacity = useSharedValue(0)
 
   const { data: groceryListTest } = useGroceries()
   const updateCompletedMutation = useUpdateGroceryCompleted()
@@ -54,6 +57,16 @@ export const HomeScreen = () => {
     }
   }, [groceryListTest])
 
+  useEffect(() => {
+    if (!finalLoading) {
+      opacity.value = withTiming(1, { duration: 300 })
+    }
+  }, [finalLoading])
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }))
+
   function renderItem({ item }: { item: Grocery }) {
     return (
       <GroceryCard
@@ -77,19 +90,21 @@ export const HomeScreen = () => {
 
   return (
     <SafeAreaView className="flex-1">
-      <View className="mt-4 flex-1 px-4">
-        <FlashList estimatedItemSize={96} data={groceryList} renderItem={renderItem} />
-      </View>
+      <Animated.View style={animatedStyle} className="flex-1">
+        <View className="mt-4 flex-1 px-4">
+          <FlashList estimatedItemSize={96} data={groceryList} renderItem={renderItem} />
+        </View>
 
-      <View className="mb-8 px-4">
-        <Button
-          size="lg"
-          onPress={() => navigation.navigate('GroceryEdit', {})}
-          disabled={finalLoading}
-        >
-          <Text className="text-white">New item</Text>
-        </Button>
-      </View>
+        <View className="mb-8 px-4">
+          <Button
+            size="lg"
+            onPress={() => navigation.navigate('GroceryEdit', {})}
+            disabled={finalLoading}
+          >
+            <Text className="text-white">New item</Text>
+          </Button>
+        </View>
+      </Animated.View>
     </SafeAreaView>
   )
 }
